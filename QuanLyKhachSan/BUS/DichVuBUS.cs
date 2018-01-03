7540e2cc255266db08ed437058e26401e601fae2
DESKTOP-2PFPM2E\TranThai <thaitran101096@gmail.com>
2018-01-03 21:49:56 +0700

    Update su dung dich vu lan 1

---
+++ QuanLyKhachSan/BUS/DichVuBUS.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class DichVuBUS
+    {
+        public List<DichVu> DanhSachDichVu()
+        {
+            return new DichVuDAL().DanhSachDichVu();
+        }
+        public List<DichVu> TimKiemDichVu(string chuoitimkiem)
+        {
+            return new DichVuDAL().TimKiemDichVu(chuoitimkiem);
+        }
+    }
+}
---
+++ QuanLyKhachSan/DAL/DichVuDAL.cs
@@ -0,0 +1,45 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class DichVuDAL
+    {
+        public List<DichVu> DanhSachDichVu()
+        {
+            List<DichVu> list = new List<DichVu>();
+            DataTable dt = new DataTable();
+            dt = DataProvider.Instance.GetDataQuerry("select *from DichVu");
+            foreach(DataRow dr in dt.Rows)
+            {
+                DichVu d = new DichVu();
+                d.Madichvu = dr["madichvu"].ToString();
+                d.Tendichvu = dr["tendichvu"].ToString();
+                d.Dongia =Decimal.Parse( dr["dongia"].ToString());
+                list.Add(d);
+            }
+            return list;
+        }
+        public List<DichVu> TimKiemDichVu(string chuoitimkiem)
+        {
+
+            List<DichVu> list = new List<DichVu>();
+            DataTable dt = new DataTable();
+            dt = DataProvider.Instance.GetDataQuerry("select * from DichVu where tendichvu like N'%" + chuoitimkiem + "%'");
+            foreach (DataRow dr in dt.Rows)
+            {
+                DichVu d = new DichVu();
+                d.Madichvu = dr["madichvu"].ToString();
+                d.Tendichvu = dr["tendichvu"].ToString();
+                d.Dongia = Decimal.Parse(dr["dongia"].ToString());
+                list.Add(d);
+            }
+            return list;
+        }
+    }
+}
